8703974be3db0386c052916d5ead45c3a763e17a
(no author) <(no author)@9e03e964-762d-0410-a424-824683d530f5>
2007-05-12 01:40:35 +0000

    add tests for resource w/o a locale
    git-svn-id: http://svn.artweb-design.de/plugins/rails/localized_url_helpers@35 9e03e964-762d-0410-a424-824683d530f5

--- test/localized_url_helpers_test.rb
+++ test/localized_url_helpers_test.rb
@@ -15,6 +15,7 @@ end
 ActionController::Routing::Routes.draw do |map|
   map.resources :articles, :path_prefix => ":locale"
   map.resources :features, :path_prefix => ":locale"
+  map.resources :users
 end
 
 class StubController < ActionController::Base
@@ -43,20 +44,38 @@ class LocalizedUrlHelperTest < Test::Unit::TestCase
     assert_equal "/en/features", @controller.features_path
   end
   
-  def self.define_tests(name)
-    tests = [ :nothing,               "/en/#{name}s/1", [ ],
-              :positioned_same_id,    "/en/#{name}s/1", [ 1 ],
-              :positioned_other_id,   "/en/#{name}s/9", [ 9 ],
-              :named_id,              "/en/#{name}s/9", [ :id => 9 ],
-              :named_locale,          "/fr/#{name}s/1", [ :locale => 'fr' ],
-              :named_locale_and_id,   "/fr/#{name}s/9", [ :locale => 'fr', :id => 9 ] ]
-    tests.chunks(3).each do |param_name, expected, path_args|
-      define_method("test_#{name}_path_with_#{param_name}".to_sym) do 
-        assert_equal expected, @controller.method("#{name}_path").call(*path_args) 
+  def test_collection_path_without_locale
+    assert_equal '/users', @controller.method('users_path').call
+  end
+  
+  class << self  
+    def tests(name, with_locale) 
+      case with_locale
+      when true
+        [ :nothing,               "/en/#{name}s/1", [ ],
+          :positioned_same_id,    "/en/#{name}s/1", [ 1 ],
+          :positioned_other_id,   "/en/#{name}s/9", [ 9 ],
+          :named_id,              "/en/#{name}s/9", [ :id => 9 ],
+          :named_locale,          "/fr/#{name}s/1", [ :locale => 'fr' ],
+          :named_locale_and_id,   "/fr/#{name}s/9", [ :locale => 'fr', :id => 9 ] ]     
+      when false
+        [ :nothing,               "/users/1", [ ],
+          :positioned_same_id,    "/users/1", [ 1 ],
+          :positioned_other_id,   "/users/9", [ 9 ],
+          :named_id,              "/users/9", [ :id => 9 ], ]      
+      end.chunks(3)
+    end
+  
+    def define_tests(name, with_locale)                              
+      tests(name, with_locale).each do |param_name, expected, path_args|
+        define_method("test_#{name}_path_with_#{param_name}".to_sym) do 
+          assert_equal expected, @controller.method("#{name}_path").call(*path_args) 
+        end
       end
     end
   end
   
-  self.define_tests(:article)
-  self.define_tests(:feature)
+  self.define_tests(:article, true)
+  self.define_tests(:feature, true)
+  self.define_tests(:user, false)
 end
